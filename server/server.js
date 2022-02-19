a9fc3188621148e3e87423dc35e08b89f95b177f
Austin Cuddeback <ajcuddeback@gmail.com>
2022-02-19 14:35:16 -0500

    connected front and backend for dev

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// If in production, use the client/build
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+};
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`APP is listening on port ${PORT}`)
+});
